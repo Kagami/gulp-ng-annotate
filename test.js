b6593dda0e2644e68bde1dc2eb2f0d103894685f
Kagami Hiiragi <kagami@genshiken.org>
2014-05-15 21:34:38 +0400

    Test for bad input

--- test.js
+++ test.js
@@ -6,6 +6,7 @@ var ngAnnotate = require("./index");
 
 var ORIGINAL = 'angular.module("test"); m.directive("foo", function($a, $b) {});';
 var TRANSFORMED = 'angular.module("test"); m.directive("foo", ["$a", "$b", function($a, $b) {}]);';
+var BAD_INPUT = 'angular.module("test").directive("foo", function$a, $b) {});';
 
 describe("gulp-ng-annotate", function() {
 
@@ -29,6 +30,17 @@ describe("gulp-ng-annotate", function() {
     });
 
     stream.write(new gutil.File({contents: new Buffer(TRANSFORMED)}));
+  });
+
+  it("should emit PluginError on bad input", function (done) {
+    var stream = ngAnnotate();
+
+    try {
+        stream.write(new gutil.File({contents: new Buffer(BAD_INPUT)}));
+    } catch (err) {
+        assert(err instanceof gutil.PluginError);
+        done();
+    }
   });
 
 });
